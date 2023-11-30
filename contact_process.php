a115be7b60d282ca1232793248c2cd7d41aa3dc5
Louis Wharmby <louiswharmby@gmail.com>
2023-11-30 22:27:23 +0000

    Adding google map, updating contact form and aligning headers/footers

--- contact_process.php
+++ contact_process.php
@@ -1,11 +1,11 @@
 <?php
 
-    $to = "spn8@spondonit.com";
+    $to = "beccy@beccysalpinechefservicechamonix.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
+    $subjectFromForm = $_REQUEST['subject'];
     $number = $_REQUEST['number'];
-    $cmessage = $_REQUEST['message'];
+    $message = $_REQUEST['message'];
 
     $headers = "From: $from";
 	$headers = "From: " . $from . "\r\n";
@@ -13,22 +13,22 @@
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $subject = "You have a message from your Bitmap Photography.";
+    $logo = 'img/beccysAlpineChefServicesChamonixLogo.png';
+    $link = 'https://www.beccysalpinechefservicechamonix.com';
 
-    $logo = 'img/logo.png';
-    $link = '#';
+	$subjectForEmail = "Enquiry via the website from " . $name
 
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
+	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Website enquiry</title></head><body>";
 	$body .= "<table style='width: 100%;'>";
 	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
+	$body .= "<a href='{$link}'><img src='{$logo}' width='100px' alt=''></a><br><br>";
 	$body .= "</td></tr></thead><tbody><tr>";
 	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
 	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
 	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
+	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$subjectFromForm}</td></tr>";
 	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
+	$body .= "<tr><td colspan='2' style='border:none;'>{$message}</td></tr>";
 	$body .= "</tbody></table>";
 	$body .= "</body></html>";
 
